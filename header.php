d5ae662e8a71e97997d4e71513d18a4875452df0
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-08-02 00:01:16 -0400

    pulled a bunch of gunk out of the head and added out l33t acii art

--- header.php
+++ header.php
@@ -8,23 +8,47 @@
  * @subpackage Twenty_Eleven
  * @since Twenty Eleven 1.0
  */
-?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php
+?>
+ <!DOCTYPE html>
+ <html>
+ <head> 
+   <meta charset="utf-8">
+   <!--
+
+      bbbbbbbb                                                                                                       
+      b::::::b                                                                                                       
+      b::::::b                                                                                                       
+      b::::::b                                                                                                       
+       b:::::b                                                                                                       
+       b:::::bbbbbbbbb       ooooooooooo       cccccccccccccccc   ooooooooooo   uuuuuu    uuuuuu ppppp   ppppppppp   
+       b::::::::::::::bb   oo:::::::::::oo   cc:::::::::::::::c oo:::::::::::oo u::::u    u::::u p::::ppp:::::::::p  
+       b::::::::::::::::b o:::::::::::::::o c:::::::::::::::::co:::::::::::::::ou::::u    u::::u p:::::::::::::::::p 
+       b:::::bbbbb:::::::bo:::::ooooo:::::oc:::::::cccccc:::::co:::::ooooo:::::ou::::u    u::::u pp::::::ppppp::::::p
+       b:::::b    b::::::bo::::o     o::::oc::::::c     ccccccco::::o     o::::ou::::u    u::::u  p:::::p     p:::::p
+       b:::::b     b:::::bo::::o     o::::oc:::::c             o::::o     o::::ou::::u    u::::u  p:::::p     p:::::p
+       b:::::b     b:::::bo::::o     o::::oc:::::c             o::::o     o::::ou::::u    u::::u  p:::::p     p:::::p
+       b:::::b     b:::::bo::::o     o::::oc::::::c     ccccccco::::o     o::::ou:::::uuuu:::::u  p:::::p    p::::::p
+       b:::::bbbbbb::::::bo:::::ooooo:::::oc:::::::cccccc:::::co:::::ooooo:::::ou:::::::::::::::uup:::::ppppp:::::::p
+       b::::::::::::::::b o:::::::::::::::o c:::::::::::::::::co:::::::::::::::o u:::::::::::::::up::::::::::::::::p 
+       b:::::::::::::::b   oo:::::::::::oo   cc:::::::::::::::c oo:::::::::::oo   uu::::::::uu:::up::::::::::::::pp  
+       bbbbbbbbbbbbbbbb      ooooooooooo       cccccccccccccccc   ooooooooooo       uuuuuuuu  uuuup::::::pppppppp    
+                                                                                                   p:::::p                                                                                                            
+                          ______                                                                   p:::::p    
+                         (  / /                      _/_                          _/_             p:::::::p   
+                           / /_  _    _ _ _   __ (   /     _,  __,  _ _   _,  (   /  _  _         p:::::::p   
+                         _/ / /_(/_  / / / /_(_)/_)_(__   (_)_(_/(_/ / /_(_)_/_)_(__(/_/ (_       p:::::::p   
+                                                           /|             /|                      ppppppppp
+                                                          (/             (/                    
+             ___               __,                    
+            ( /               (           o       _/_ 
+             / __,  _  ,___,   `.  _, _  ,   ,_   /    _, __ _ _ _    ,_   __,  _ _   __  ,
+           _/_(_/(_/ |/ (_/(_(___)(__/ (_(__/|_)_(__  (__(_)/ / / /__/|_)_(_/(_/ / /_/ (_/_
+          //                                /|                       /|                 /  o
+         (/                                (/                       (/                 '
+
+   -->
+
+   <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
 	 */
@@ -45,9 +69,7 @@
 		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
 
 	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
@@ -72,42 +94,7 @@
 	<header id="branding" role="banner">
 			<hgroup>
 				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
-				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
-
-			<?php
-				// Check to see if the header image has been removed
-				$header_image = get_header_image();
-				if ( ! empty( $header_image ) ) :
-			?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				<?php
-					// The header image
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else : ?>
-					<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-				<?php endif; // end check for featured image or standard header ?>
-			</a>
-			<?php endif; // end check for removed header image ?>
-
-			<?php
-				// Has the text been hidden?
-				if ( 'blank' == get_header_textcolor() ) :
-			?>
-				<div class="only-search<?php if ( ! empty( $header_image ) ) : ?> with-image<?php endif; ?>">
-				<?php get_search_form(); ?>
-				</div>
-			<?php
-				else :
-			?>
-				<?php get_search_form(); ?>
-			<?php endif; ?>
 
       <nav id="access" role="navigation">
         <h3 class="assistive-text">Main menu</h3>
