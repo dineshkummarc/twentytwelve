f8eba1448c6c399fa709d0342512c75acabd66e7
boaz <boaz@bocoup.(none)>
2011-08-02 04:25:20 +0000

    removing titles from pages

--- content-page.php
+++ content-page.php
@@ -8,11 +8,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-
+<article id="post-<?php the_ID(); ?>" >
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
