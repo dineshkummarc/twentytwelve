b9b508893c5ab14b787f26ceabfce9a1e8d25341
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-08-06 13:15:13 -0400

    adding footer content

--- footer.php
+++ footer.php
@@ -18,13 +18,44 @@
 				/* A sidebar in the footer? Yep. You can can customize
 				 * your footer with three columns of widgets.
 				 */
-				get_sidebar( 'footer' );
+				# get_sidebar( 'footer' );
 			?>
 
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
+    	<div class="container">	
+    		<div class="one-third column">
+    		  <h3>Bocoup</h3>
+    		  <ul>
+    		    <li>Email: admin@bocoup.com</li>
+    		    <li>Call: 617-379-2752</li>
+    		    <li>Visit: 355 Congress St, Boston</li>
+    		  </ul>
+    		  
+    		  <hr>
+    		  
+    		  <p>
+    		    All code on this website is <a href="http://code.bocoup.com/license/">Open Source</a>. Want to work at Bocoup? <a href="http://workat.bocoup.com/#apply">Then Apply</a>.
+    		  </p>
+
+    		</div>
+    		<div class="one-third column">
+
+    		</div>
+    		<div class="one-third column">
+
+    		</div>
+
+        <hr>
+        
+    		<div class="sixteen columns">
+          <a class="center html5" href="http://www.w3.org/html/logo/">
+            <img src="http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png" height="64" alt="HTML5 Powered with Connectivity / Realtime, CSS3 / Styling, Device Access, Graphics, 3D &amp; Effects, Multimedia, Performance &amp; Integration, Semantics, and Offline &amp; Storage" title="HTML5 Powered with Connectivity / Realtime, CSS3 / Styling, Device Access, Graphics, 3D &amp; Effects, Multimedia, Performance &amp; Integration, Semantics, and Offline &amp; Storage">
+          </a>
+          <a href="http://bostonbuilt.org"><img height="54" src="http://bostonbuilt.org/icon.php?q=built_in_boston_color_fancy.png&amp;u=www.bocoup.com"></a>
+    		</div>
+    	</div><!-- container -->
+
+      
+      
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
