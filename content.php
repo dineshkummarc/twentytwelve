5cf57c612cb281fe5c1a709902ea6e3a6a6014bd
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-08-16 23:46:34 -0400

    updated twentyeleven_posted_on to be more bocoup

--- content.php
+++ content.php
@@ -7,17 +7,12 @@
  * @since Twenty Eleven 1.0
  */
 ?>
-  <article id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a> &rarr;</h2>
 
-  	<p class="entry-date fancy">
-      Posted by
-      <span class="author vcard">
-        <?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?>
-      </span>
-      on
-      <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a> &rarr;</h2>
+		<p class="entry-date fancy">
+      <abbr class="published">
         <?php twentyeleven_posted_on(); ?>
       </abbr>
     </p>
-  </article><!-- .post -->
+	</article><!-- #post-<?php the_ID(); ?> -->
