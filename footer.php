ab50760b90daaa540fa390c847d7e6981989a9ec
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-08-16 23:07:05 -0400

    testing multi footer

--- footer.php
+++ footer.php
@@ -9,43 +9,33 @@
  * @since Twenty Eleven 1.0
  */
 ?>
-    <hr>
-    <hr class="clear space">
-    
-    <div class="container">
-  		<div class="one-third column">          
-  		  <p>
-  		    admin@bocoup.com</br>
-  		    617-379-2752</br>
-  		    355 Congress St, Boston, MA
-  		  </p>          
-  		</div>
-  		<div class="one-third column">
-        <ul>
-          <li><a href="http://store.bocoop.com">Store</a></li>
-          <li><a href="http://gamelab.bocoop.com">Gamelab</a></li>
-          <li><a href="http://ngc.bocoop.com">New Game Conference</a></li>
-        </ul>
-  		</div>
-  		<div class="one-third column">
-  		  <p>
-  		    All code on this website is <a href="http://code.bocoup.com/license/">Open Source</a>. Want to work at Bocoup? <a href="http://workat.bocoup.com/#apply">Then Apply</a>.
-  		  </p>
-  		</div>
-  	</div><!-- container -->
-  	
+
 	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<?php
+				/* A sidebar in the footer? Yep. You can can customize
+				 * your footer with three columns of widgets.
+				 */
+				 
+				global $wpdb;
+        $current_blogid = $wpdb->blogid;
+        $wpdb->set_blog_id(1);
+        wp_cache_reset();
+        get_sidebar( 'footer' );
+        $wpdb->set_blog_id($current_blogid);
+        wp_cache_reset();
+        unset($current_blogid);
+        
+				
+			?>
+
+	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<hr class="clear space">
-
-<div class="center">
-  <a class="center html5" href="http://www.w3.org/html/logo/">
-    <img src="http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png" height="64" alt="HTML5 Powered with Connectivity / Realtime, CSS3 / Styling, Device Access, Graphics, 3D &amp; Effects, Multimedia, Performance &amp; Integration, Semantics, and Offline &amp; Storage" title="HTML5 Powered with Connectivity / Realtime, CSS3 / Styling, Device Access, Graphics, 3D &amp; Effects, Multimedia, Performance &amp; Integration, Semantics, and Offline &amp; Storage">
-  </a>
-  <a href="http://bostonbuilt.org"><img height="54" src="http://bostonbuilt.org/icon.php?q=built_in_boston_color_fancy.png&amp;u=www.bocoup.com"></a>
-</div>
-<hr class="clear space">
+<?php wp_footer(); ?>
 
 </body>
 </html>
+
